540bf3b4853449961498580d9aa2b61277f5e836
sarupmoni <93664377+sarupmoni@users.noreply.github.com>
2026-03-25 13:51:00 +0530

    [INJIVER-1585] - refactor code to use ES256SignatureVerifierImpl to hande ES256 signature
    Signed-off-by: sarupmoni <93664377+sarupmoni@users.noreply.github.com>

--- vc-verifier/kotlin/vcverifier/src/main/java/io/mosip/vercred/vcverifier/signature/SignatureFactory.kt
+++ vc-verifier/kotlin/vcverifier/src/main/java/io/mosip/vercred/vcverifier/signature/SignatureFactory.kt
@@ -6,10 +6,7 @@ import io.mosip.vercred.vcverifier.constants.CredentialVerifierConstants.JWS_ES2
 import io.mosip.vercred.vcverifier.constants.CredentialVerifierConstants.JWS_PS256_SIGN_ALGO_CONST
 import io.mosip.vercred.vcverifier.constants.CredentialVerifierConstants.JWS_RS256_SIGN_ALGO_CONST
 import io.mosip.vercred.vcverifier.exception.SignatureNotSupportedException
-import io.mosip.vercred.vcverifier.signature.impl.ED25519SignatureVerifierImpl
-import io.mosip.vercred.vcverifier.signature.impl.ES256KSignatureVerifierImpl
-import io.mosip.vercred.vcverifier.signature.impl.PS256SignatureVerifierImpl
-import io.mosip.vercred.vcverifier.signature.impl.RS256SignatureVerifierImpl
+import io.mosip.vercred.vcverifier.signature.impl.*
 
 class SignatureFactory {
 
@@ -19,7 +16,7 @@ class SignatureFactory {
             JWS_RS256_SIGN_ALGO_CONST -> RS256SignatureVerifierImpl()
             JWS_EDDSA_SIGN_ALGO_CONST -> ED25519SignatureVerifierImpl()
             JWS_ES256K_SIGN_ALGO_CONST -> ES256KSignatureVerifierImpl()
-            JWS_ES256_SIGN_ALGO_CONST -> ES256KSignatureVerifierImpl()
+            JWS_ES256_SIGN_ALGO_CONST -> ES256SignatureVerifierImpl()
             else -> throw SignatureNotSupportedException("Unsupported jws signature algorithm")
         }
     }
